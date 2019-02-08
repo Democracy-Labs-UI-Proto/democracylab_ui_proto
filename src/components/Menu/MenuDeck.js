03781450daadb125439b0e6db8caf4c786c80c1b
Maxwell Rediker <maxwell.rediker@gmail.com>
2019-02-08 14:20:59 -0800

    added noscroll and dark overlay on menu open

--- src/components/Menu/MenuDeck.js
+++ src/components/Menu/MenuDeck.js
@@ -10,14 +10,19 @@ export default class MenuDeck extends Component {
   }
 
   menuClick = () => {
-    this.setState(prevState => ({
-      show: !prevState.show
-    }));
+    this.setState(
+      prevState => ({
+        show: !prevState.show
+      }),
+      () => {
+        document.getElementsByTagName('body')[0].classList.toggle('show');
+      }
+    );
   };
 
   render() {
     return (
-      <header>
+      <header className={`${this.state.show ? `show` : ``}`}>
         <MenuHamburger menuClick={this.menuClick} show={this.state.show} />
         <nav className={`menu ${this.state.show ? `show` : ``}`}>
           <ul className={`menu-nav gradient-blue ${this.state.show ? `show` : ``}`}>
--- src/components/Roles/RolesDeck.js
+++ src/components/Roles/RolesDeck.js
@@ -10,8 +10,10 @@ export default class Roles extends Component {
     // TODO change roles-container class to be just container (flex)
     return (
       <div className="roles-container">
-        <h2 className="title small-space">Available Roles</h2>
-        <ul id="roles" className="roles-list big-space">
+        <h2 id="roles" className="title small-space">
+          Available Roles
+        </h2>
+        <ul className="roles-list big-space">
           {this.props.roles.map((role, idx) => (
             <RoleCard key={idx} title={role.title} content={role.description} />
           ))}
